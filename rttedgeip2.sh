e4fd7e8afab5b0efe274334036974eabc03b668b
KUN HO KO <33794164+bedro96@users.noreply.github.com>
2018-08-30 14:58:05 +0900

    Update rttedgeip2.sh

--- rttedgeip2.sh
+++ rttedgeip2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 systemctl stop firewalld
-rm edgeiplist
+rm edgeiplist2
 wget https://raw.githubusercontent.com/bedro96/linuxscripts/master/edgeiplist2
 input="./edgeiplist2"
 while IFS= read -r var
