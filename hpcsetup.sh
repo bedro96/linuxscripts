c7db1a9ffa05f8e6897535443aaf27752a19ec92
KUN HO KO <33794164+bedro96@users.noreply.github.com>
2019-03-14 14:20:52 +0900

    Create hpcsetup.sh

---
+++ hpcsetup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh 
+set -ex 
+echo "Updating packages ..." 
+sudo yum -y update 
+sudo yum -y group install "Development tools" 
+echo "Installing lftp" 
+sudo yum -y install lftp 
+echo "Installing p7zip" 
+sudo wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/p7zip-16.02-10.el7.x86_64.rpm 
+sudo wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/7/x86_64/Packages/p/p7zip-plugins-16.02-10.el7.x86_64.rpm 
+sudo rpm -U --quiet p7zip-16.02-10.el7.x86_64.rpm 
+sudo rpm -U --quiet p7zip-plugins-16.02-10.el7.x86_64.rpm 
+echo "Installing cmake 3.12.2" 
+sudo yum -y install epel-release 
+sudo yum -y install cmake3 
+sudo yum -y remove cmake 
+sudo ln -s /usr/bin/cmake3 /usr/bin/cmake 
